fb713d13725a20786845089e43ed7856bd95da9d
Mirnes Becirbegovic <mirnesbb@gmail.com>
2022-06-13 16:12:31 +0200

    first_commit

---
+++ script.js
@@ -0,0 +1,86 @@
+let crashRide = document.querySelector("#crash-ride"); 
+let hiHatTop = document.querySelector("#hihat-top");
+
+const animateCrashOrRide = (event) => {
+    crashRide.style.transform = "rotate(0deg) scale(1.5)";
+};
+
+const animateHiHatClosed = (event) => {
+    hiHatTop.style.top = "171px";
+};
+
+
+
+window.addEventListener("keydown", (event) => {
+
+    let code = event.keyCode;
+
+    let keyElement = document.querySelector(`div[data-key="${code}"]`);
+
+    // if(keyElement) {
+
+    //     console.log("postoji");
+
+    // } else {
+        
+    //     console.log("ne postoji");
+    // }
+
+    if(!keyElement) {
+        // console.log("ne postoji");
+
+        return;
+    }
+
+    console.log(keyElement);
+
+    let audio = document.querySelector(`audio[data-key = "${code}"]`);
+    audio.currentTime = 0;
+    audio.play();
+
+    switch(code) {
+        case 69:
+        case 82:
+            animateCrashOrRide();
+            break;
+        case 75:
+        case 73:
+            animateHiHatClosed();
+            break;
+    }
+
+    keyElement.classList.remove("playing");
+
+});
+
+const remvoeCrashRideTransition = e => {
+
+    if(e.propertyName !== "transform") return;
+
+    e.target.style.transform = "rotate(-7.2deg) scale(1.5)"
+
+};
+
+const removeHiHatTopTransition = e => {
+    if(e.propertyName !== "top") return;
+    
+    e.target.style.top = "166px";
+};
+
+const removeKeyTransition = e => {
+    if(e.propertyName !== "transform") return;
+
+    e.target.classList.remove("playing");
+}
+
+let drumKeys = document.querySelectorAll(".key"); 
+
+drumKeys.forEach((key) => {
+
+    key.addEventListener("transitionend", removeKeyTransition);
+
+});
+
+crashRide.addEventListener("transitionend" , remvoeCrashRideTransition);
+
+hiHatTop.addEventListener("transitionend" , removeHiHatTopTransition);
